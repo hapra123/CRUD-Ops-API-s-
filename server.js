0f78490bf89f46f287f9d581cd219482c46c1991
hapra123 <hardikprabhu123@gmail.com>
2023-09-03 14:40:49 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express= require('express')
+const app=express()
+
+app.get('/',(req,res)=>{
+  res.send('Hello node api')
+
+})
+
+
+
+app.listen(3000,()=>{
+ console.log('Node API is running on port 3000')
+
+})
